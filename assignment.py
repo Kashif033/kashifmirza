3a846dc29878c637132d3fe625d9924c032add5e
Kashif033 <125971539+Kashif033@users.noreply.github.com>
2023-10-09 14:32:31 +0500

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,23 @@
+#qno1
+for num in num_list:
+print(num)
+#qno2
+if(Num>45):
+    print(num)
+    #QNO3
+    if(num>45):
+        print("over 45")
+        else
+        print("under 45")
+        #qno4
+    for index num in enumerate (num_list)
+    if(num==36):
+        print("number found at position",index)
+        #qno5,6,7,8
+        count=0
+        for index num in enumerate(num_list):
+            if(num==36):
+                count+=1
+                print("number found at position",index)
+                break
+            print("total value at counter =",count)
